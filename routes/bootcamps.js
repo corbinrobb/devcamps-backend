6321d02fb95bb78cbe6ee42b990d3f155be2b483
Corbin Robb <corbin@Corbins-MacBook-Pro.local>
2021-02-20 19:48:17 -0700

    Create controllers folder and move routes for bootcamps router to it

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,27 +1,17 @@
 const router = require("express").Router();
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all" });
-});
-
-router.get("/:id", (req, res) => {
-  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
-});
-
-router.post("/", (req, res) => {
-  res.status(201).json({ success: true, msg: "Create new bootcamp" });
-});
-
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(updateBootcamp);
 
 module.exports = router;
