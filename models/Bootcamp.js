ac7ec99c47c839086da6520858204be77705d1f7
Corbin Robb <corbin@Corbins-MacBook-Pro.local>
2021-02-23 14:50:48 -0700

    Add slug and geocoder DB middleware and seeder files and funtions

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -96,6 +98,30 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode and create location field
+BootcampSchema.pre("save", async function (next) {
+  const [loc] = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc.longitude, loc.latitude],
+    formattedAddress: loc.formattedAddress,
+    street: loc.streetName,
+    city: loc.city,
+    state: loc.stateCode,
+    zipcode: loc.zipcode,
+    country: loc.countryCode,
+  };
+
+  this.address = undefined;
+  next();
 });
 
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
